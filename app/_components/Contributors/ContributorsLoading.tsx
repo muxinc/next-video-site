59c8645edecab9ae75fbdb4e2e325a1f3cfbbfa4
Darius Cepulis <dcepulis@mux.com>
2023-10-30 11:40:29 -0500

    lint-staged & formatting

--- app/_components/Contributors/ContributorsLoading.tsx
+++ app/_components/Contributors/ContributorsLoading.tsx
@@ -1,12 +1,12 @@
 export default async function ContributorsLoading() {
   return (
     <ul className="flex flex-wrap items-center justify-center md:justify-start">
-      <li className="w-48 h-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.0s' }} />
-      <li className="w-48 h-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.1s' }} />
-      <li className="w-48 h-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.2s' }} />
-      <li className="w-48 h-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.3s' }} />
-      <li className="w-48 h-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.4s' }} />
-      <li className="w-48 h-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.5s' }} />
+      <li className="h-48 w-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.0s' }} />
+      <li className="h-48 w-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.1s' }} />
+      <li className="h-48 w-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.2s' }} />
+      <li className="h-48 w-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.3s' }} />
+      <li className="h-48 w-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.4s' }} />
+      <li className="h-48 w-48 animate-pulse rounded-full bg-gray-23" style={{ animationDelay: '0.5s' }} />
     </ul>
   );
 }
